994a5089edb30da834c929dcb443a60cf6f1a99f
mac <0924>
2024-10-11 23:07:53 +0800

    intro

---
+++ src/main/java/com/kerwim/frameworklearnspringboot/FrameworkLearnSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.kerwim.frameworklearnspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworkLearnSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrameworkLearnSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kerwim/frameworklearnspringboot/FrameworkLearnSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kerwim.frameworklearnspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameworkLearnSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
